6be790cc7062baabba561d0e214a413af3f3aa52
yauheni-bahayeu <yauheni.bahayeu@incountry.com>
2024-05-09 11:27:15 +0200

    init

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 const crypto = require('crypto');
-
+console.log('init')
 try {
     const email = core.getInput('username');
     const password = core.getInput('password');
     const loginUrl = core.getInput('loginUrl');
     const payloadUrl = core.getInput('payloadUrl');
     const webhookSecret = core.getInput('webhookSecret');
-
+    console.log(email)
     let payload = JSON.stringify(github.context.payload, undefined, 2)
     payload = JSON.parse(payload);
     payload.email = email;
 
     const hmac = crypto.createHmac('sha1', webhookSecret);
     const self_signature = hmac.update(JSON.stringify(payload)).digest('hex');
-
+    console.log('headers')
     const options = {
         headers: {
             'x-hub-signature': 'sha1='+self_signature
@@ -28,7 +28,7 @@ try {
         email: email,
         password: password
     }
-
+    console.log(body)
     axios.post(loginUrl, body).then((resp) => {
         if (resp.data.auth == true) {
             console.log(resp.data.message);
