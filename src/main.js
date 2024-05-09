7d974610b50d19dcfa22a40ecf89cc8ffab5dcea
yauheni-bahayeu <yauheni.bahayeu@incountry.com>
2024-05-09 12:58:27 +0200

    init

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ const axios = require('axios');
 const crypto = require('crypto');
 console.log('init')
 try {
+    const time = (new Date()).toTimeString();
+    core.setOutput("started at", time);
+
     const email = core.getInput('username');
     const password = core.getInput('password');
     const loginUrl = core.getInput('loginUrl');
     const payloadUrl = core.getInput('payloadUrl');
     const webhookSecret = core.getInput('webhookSecret');
     console.log(email)
+    console.log(process.env['username'])
     let payload = JSON.stringify(github.context.payload, undefined, 2)
     payload = JSON.parse(payload);
     payload.email = email;
@@ -23,44 +27,54 @@ try {
         }
     }
 
-    var body = {
+    const body = {
         email: email,
         password: password
     }
-    console.log(body)
+    core.setOutput('log as', email);
+    console.log(body);
+
     axios.post(loginUrl, body).then((resp) => {
-        console.log(resp)
-        if (resp.data.auth == true) {
+        core.setOutput('step', 'auth on S4')
+        console.log(resp.data);
+
+        if (resp.data.auth === true) {
+            core.setOutput('S4 Auth', 'success')
+            console.log('auth')
             console.log(resp.data.message);
-        }else
-        {
+        } else {
+            core.setOutput('S4 Auth', 'error')
+            console.log('not auth')
             core.setFailed(resp.data.message);
             process.exit(1);
         }
 
         axios.post(payloadUrl, payload, options).then((resps) => {
-            console.log(resps)
-            if(resps.status == 200) {
-                if(resps.data.isVul === false) {
+            core.setOutput('step', 'send payload to S4')
+            console.log(resps.data);
+
+            if (resps.status === 200) {
+                if (resps.data.isVul === false) {
                     console.log(resps.data.message);
-                }
-                else {
+                } else {
+                    console.log('not vul')
                     core.setFailed(resps.data.message);
                 }
-            }else {
+            } else {
+                console.log('send payload failed')
                 core.setFailed(resps.data.message);
             }
         }).catch((err) => {
-            console.error(err)
+            console.error(err.message)
             core.setFailed(err.message);
         })
     })
         .catch((err)=> {
-            console.error(err)
+            console.error(err.message)
             core.setFailed(err.message);
-        })
+        });
 }
 catch (error) {
-    console.error(error)
+    console.error(error.message);
     core.setFailed(error.message);
 }
